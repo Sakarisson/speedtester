a35815ae6a22fa240521a345183bc684da80286a
Kristian Sakarisson <kristian@sakarisson.com>
2017-03-05 01:17:36 +0000

    Added some barebones functionality

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
+require('dotenv').config()
+const mysql = require('mysql')
 const speedTest = require('speedtest-net')
 
-const test = speedTest({
-  maxTime: 60000, // 1 minute
-  serverId: 462   // Faroese Telecom server
-})
+function checkSpeed(callback) {
+  const test = speedTest({
+    maxTime: 60000, // 1 minute
+    serverId: 462   // Faroese Telecom server
+  })
 
-test.on('downloadprogress', (progress) => {
-  console.log('Download progress:', progress)
-})
+  test.on('downloadprogress', (progress) => {
+    console.log('Download progress:', progress)
+  })
 
-test.on('uploadprogress', (progress) => {
-  console.log('Upload progress:', progress)
-})
+  test.on('uploadprogress', (progress) => {
+    console.log('Upload progress:', progress)
+  })
 
-test.on('data', (data) => {
-  console.dir(data)
-})
+  test.on('data', (data) => {
+    console.dir(data)
+  })
 
-test.on('error', (err) => {
-  console.error(err)
+  test.on('error', (err) => {
+    console.error(err)
+  })
+}
+
+function connectToDatabase(callback) {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+  })
+
+  connection.connect((err) => {
+    if(err) {
+      callback(err)
+      return
+    }
+    callback(null, connection)
+  })
+}
+
+connectToDatabase((err, connection) => {
+  if(err) {
+    console.error(err)
+    return
+  }
+  connection.query(`SELECT * FROM measurements;`, (err, result) => {
+    if(err) {
+      console.error(err)
+      return
+    }
+    console.log(result)
+  })
 })
